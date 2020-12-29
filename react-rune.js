cf63437e182ecacbe2fcae0d5246de0ae16bea54
Romello Goodman <romellogood@gmail.com>
2020-12-28 21:49:58 -0500

    Create react-rune.js

---
+++ react-rune.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import RuneJS from "rune.js";
+
+const Rune = (props) => {
+  const { container = ".canvas", draw, height, width, onUpdate, play } = props;
+
+  useEffect(() => {
+    const rune = new RuneJS({
+      container,
+      height,
+      width,
+    });
+
+    window.RUNE = rune;
+
+    draw(rune);
+
+    if (onUpdate) {
+      rune.on("update", onUpdate);
+    }
+
+    if (play) {
+      rune.play();
+    } else {
+      rune.draw();
+    }
+
+    return () => {
+      // Remove the svg from the DOM
+      console.log("Rune unmount");
+
+      rune.el.remove();
+    };
+  }, [draw]);
+
+  return null;
+};
+
+export default Rune;
